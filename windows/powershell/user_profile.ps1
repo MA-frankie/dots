4cdb893a49e6b1bcc464242fed9776d1f7f04fce
Oleg Andrukhovich <oleg@andrukhovich.ru>
2023-02-04 14:23:17 +1000

    windows ps config

---
+++ windows/powershell/user_profile.ps1
@@ -0,0 +1,29 @@
+# Promt
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt Paradox
+
+# Load promt config
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
